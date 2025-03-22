9d8f0835cee5a7d68c3db258fe3be569e97abc3c
Jorge Rivero <jorge.rivero@estudiantes.utec.edu.uy>
2025-03-22 18:10:24 -0300

    cambios en html por flaticon, js operator calculadora, scss distribucion de espacio boton equal

--- script.js
+++ script.js
@@ -1,22 +1,39 @@
 const display = document.querySelector("#display");
 const buttons = document.querySelectorAll("button");
+
+let firstNumber = null;
+let isPercentageMode = false;
+
 buttons.forEach((item) => {
-    item.onclick= () => {
-        if(item.id == "clear") {
+    item.onclick = () => {
+        if (item.id === "clear") {
             display.innerText = "";
-        } else if (item.id == "backspace") {
+            firstNumber = null;
+            isPercentageMode = false;
+        } else if (item.id === "backspace") {
             let string = display.innerText.toString();
             display.innerText = string.substr(0, string.length - 1);
-        } else if (display.innerText != "" && item.id =="equal") {
-            display.innerText = eval(display.innerText);
-        } else if(display.innerText=="" && item.id=="equal"){
-            display.innerText="Null";
-            setTimeout(()=>(display.innerText=""), 2000);
-        }else{
-            display.innerText+=item.id;
+        } else if (item.id === "%") {
+            if (display.innerText !== "") {
+                firstNumber = parseFloat(display.innerText); // Guarda el primer número
+                isPercentageMode = true;
+                display.innerText = ""; // Borra la pantalla para el siguiente número
+            }
+        } else if (item.id === "equal") {
+            if (isPercentageMode && firstNumber !== null && display.innerText !== "") {
+                let percentage = parseFloat(display.innerText);
+                display.innerText = firstNumber * (percentage / 100);
+                isPercentageMode = false; // Restablece el modo porcentaje
+            } else {
+                display.innerText = eval(display.innerText);
+            }
+        } else {
+            display.innerText += item.id;
         }
     };
 });
+
+
 const themeToggleBtn = document.querySelector(".theme-toggler");
 const calculator = document.querySelector(".calculator");
 let isDark = true;
